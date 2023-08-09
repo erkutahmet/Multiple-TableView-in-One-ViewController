d1a214859505c905c195b0eb326380d2e84c25dd
itspepecura <90329328+itspepecura@users.noreply.github.com>
2023-08-09 15:51:23 +0300

    Initial Commit

---
+++ Multiple TableView in One ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multiple TableView in One ViewController
+//
+//  Created by Ahmet Erkut on 9.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
